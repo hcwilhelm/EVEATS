be4842d30899b472c5b97042a5259f902ca4f61e
NP Complete <hcwilhelm@mac.com>
2012-01-28 15:08:19 +0100

    added eveapi db

--- Backend/eveapi/models.py
+++ Backend/eveapi/models.py
@@ -1,3 +1,12 @@
+# 
+#  models.py
+#  EVEARS
+#  
+#  Created by Hans Christian Wilhelm on 2011-09-05.
+#  Copyright 2011 scienceondope.org All rights reserved.
+# 
+
+
 from django.db import models
 
 # Create your models here.
