f733aac8a06ed9ad2210050ada0542447c9413f5
Izlots <christian.schroer@coderschlampe.com>
2012-04-22 19:36:49 +0200

    moved EVE API XML fetch back to eve app....

--- Backend/common/tasks.py
+++ Backend/common/tasks.py
@@ -7,19 +7,12 @@ from functools import wraps
 # = Import django modules                                                   =
 # ===========================================================================
 from django.core.cache import cache
-from django.conf import settings
 
 # ===========================================================================
 # = Logging                                                                 =
 # ===========================================================================
 import logging
 logger = logging.getLogger(__name__)
-
-# ===========================================================================
-# = Import general python modules                                           =
-# ===========================================================================
-from lxml import etree
-import httplib
 
 # ===========================================================================
 # = Decorator to allow only one update task for each object at time         =
@@ -45,17 +38,3 @@ def locktask(function):
         logger.debug("Unlocked task: %s" % lock_id)
 
   return wrapper
-
-# =============================================================================
-# = Helper functions                                                          =
-# =============================================================================
-
-def getXMLFromEveAPI(action, params):
-  header = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-
-  connection = httplib.HTTPSConnection(settings.EVE_API_HOST, settings.EVE_API_PORT)
-  connection.request("GET", action, params, header)
-
-  xml = connection.getresponse().read()
-  connection.close()
-  return etree.fromstring(xml)
